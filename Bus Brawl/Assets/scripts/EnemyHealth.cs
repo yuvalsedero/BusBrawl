9fd0dd6ad9d59f7b8b3fe37c9e12a8ef6648be06
yuvalsedero <yuvalsede@gmail.com>
2022-05-04 14:11:35 +0300

    orc 2 has been added!

--- Bus Brawl/Assets/scripts/EnemyHealth.cs
+++ Bus Brawl/Assets/scripts/EnemyHealth.cs
@@ -5,25 +5,17 @@ using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour
 {
-    [SerializeField] float speed;
     private Rigidbody2D _rigidbody;
-
-    Vector2 positionToMoveTo;
     public Animator animator;
     float health;
     public float maxHealth;
     public GameObject healthBarUI;
     public Slider slider;
     // Start is called before the first frame update
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody2D>();
-    }
     void Start()
     {
         health = maxHealth;
         slider.value = CalculateHealth();
-        positionToMoveTo = GameObject.FindGameObjectWithTag("Bus").transform.position; // get bus entrance position
     }
     public void TakeDamage(int damage)
     {
@@ -46,8 +38,6 @@ public class EnemyHealth : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPosition = Vector2.MoveTowards(transform.position, positionToMoveTo, speed * Time.deltaTime); // enemy moving torwads bus entrance
-        _rigidbody.MovePosition(targetPosition);
         slider.value = CalculateHealth();
         if (health < maxHealth)
         {
---
+++ Bus Brawl/Assets/scripts/Orc 1/Orc1Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orc1Movement : MonoBehaviour
+
+{
+    public Animator animator;
+    private Rigidbody2D _rigidbody;
+    Vector2 positionToMoveTo;
+    [SerializeField] float speed;
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+         positionToMoveTo = GameObject.FindGameObjectWithTag("Bus").transform.position; // get bus entrance position
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 targetPosition = Vector2.MoveTowards(transform.position, positionToMoveTo, speed * Time.deltaTime); // enemy moving torwads bus entrance
+        _rigidbody.MovePosition(targetPosition);
+        animator.SetFloat("Speed", speed);
+        if(animator.GetBool("IsDead"))
+        {
+            this.enabled = false;
+        }
+    }
+}

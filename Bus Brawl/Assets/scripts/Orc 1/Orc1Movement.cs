826ae7839d8bebdf5f3e1335dd1f1be1adf0115a
yuvalsedero <yuvalsede@gmail.com>
2022-05-18 18:37:40 +0300

    ludo1 final

--- Bus Brawl/Assets/scripts/Orc 1/Orc1Movement.cs
+++ Bus Brawl/Assets/scripts/Orc 1/Orc1Movement.cs
@@ -7,7 +7,7 @@ public class Orc1Movement : MonoBehaviour
 {
     public float thrust;
     public float knockBackTime;
-
+    bool passSpawnPoint = false;
     public Animator animator;
     private Rigidbody2D body;
     Vector2 positionToMoveTo;
@@ -23,20 +23,30 @@ public class Orc1Movement : MonoBehaviour
     void Start()
     {
         enemyFirstSteps = GameObject.FindGameObjectWithTag("EnemySpawnWalk").transform.position; // get position of map before going to the bus position
-        positionToMoveTo = GameObject.FindGameObjectWithTag("Bus").transform.position; // get bus entrance position  
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 targetPosition = Vector2.MoveTowards(transform.position, enemyFirstSteps, speed * Time.deltaTime); // enemy moving torwads map entrance
+        positionToMoveTo = GameObject.FindGameObjectWithTag("Bus").transform.position; // get bus entrance position  
+        if (!passSpawnPoint)
+        {
+            // Vector2 targetPosition = Vector2.MoveTowards(transform.position, enemyFirstSteps, speed * Time.deltaTime); // enemy moving torwads map entrance
+            body.transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x, enemyFirstSteps.y), speed * Time.deltaTime);
+        }
 
-        if (body.transform.position == targetPosition.position)
-            {
-            Vector2 targetPosition = Vector2.MoveTowards(transform.position, positionToMoveTo, speed * Time.deltaTime); // enemy moving torwads bus entrance
+        if (passSpawnPoint)
+        {
+            targetPosition = Vector2.MoveTowards(transform.position, positionToMoveTo, speed * Time.deltaTime); // enemy moving torwads bus entrance
             body.transform.position = targetPosition;
-            }
+        }
         
+        if (body.position.y == enemyFirstSteps.y)
+        {
+            passSpawnPoint = true;
+        }
+
         animator.SetFloat("Speed", speed);
         if (animator.GetBool("IsDead"))
         {
@@ -46,7 +56,7 @@ public class Orc1Movement : MonoBehaviour
     public void knockback()
     {
         Rigidbody2D orc = GetComponent<Rigidbody2D>();
-        if(orc != null)
+        if (orc != null)
         {
             Vector2 difference = GameObject.FindGameObjectWithTag("Player").transform.position - transform.position;
             difference = difference.normalized * (thrust * -1);
@@ -56,7 +66,7 @@ public class Orc1Movement : MonoBehaviour
         }
         IEnumerator knockCo(Rigidbody2D orc)
         {
-            if(orc != null)
+            if (orc != null)
             {
                 yield return new WaitForSeconds(knockBackTime);
                 orc.velocity = Vector2.zero;

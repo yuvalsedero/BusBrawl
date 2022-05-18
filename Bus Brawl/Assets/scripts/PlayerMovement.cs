3fc0e794cc846b46c76536f90228dcce667b1ddd
morsedero <morsedero@gmail.com>
2022-05-18 16:44:12 +0300

    Dash

--- Bus Brawl/Assets/scripts/PlayerMovement.cs
+++ Bus Brawl/Assets/scripts/PlayerMovement.cs
@@ -5,39 +5,70 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D body;
-    float horizontal;
-    float vertical;
-    // bool Fire1;
-    [SerializeField] public float runSpeed = 20.0f;
     public Animator animator;
+    [SerializeField] public float runSpeed = 20.0f;
+
+    private Vector2 moveInput;
+    private float activeRunSpeed;
+    private float dashSpeed = 10f;
+    public float dashLength = .25f;
+    public float dashCooldown = 1f;
+    private float dashCounter = 0f;
+    private float dashCoolCounter;
+
     void Start()
     {
-        // GetComponent<Collider2D>().enabled = true;
-        body = GetComponent<Rigidbody2D>(); 
-        
+        body = GetComponent<Rigidbody2D>();
+        activeRunSpeed = runSpeed;
     }
 
     void Update() // get input of movement
     {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        moveInput.Normalize();
         
-        horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Speed", Mathf.Abs(horizontal)); //set animation of walking
-        var pos = Mathf.Round(transform.position.y);
-        
+
+        animator.SetFloat("Speed", Mathf.Abs(moveInput.x)); //set animation of walking
+
+        if (Input.GetKeyDown("space"))
+        {
+            if (dashCoolCounter <= 0 && dashCounter <= 0) // Dash
+            {
+                animator.SetTrigger("Dash");
+                activeRunSpeed = dashSpeed;
+                dashCounter = dashLength;
+            }
+        }
+
+        if (dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+
+            if (dashCounter <= 0)
+            {
+                activeRunSpeed = runSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+
+        if (dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
     }
 
     private void FixedUpdate() // set body movement to run speed and the directions from update
     {
-        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
-         if (body.velocity.x > 0)
-         {
-             transform.localScale = new Vector2(0.75f, 0.75f);
-         }
-         else if (body.velocity.x < 0)
-         {
-             transform.localScale = new Vector2(-0.75f, 0.75f);
-         }
- }
-
+        body.velocity = moveInput * activeRunSpeed;
+        if (body.velocity.x > 0)
+        {
+            transform.localScale = new Vector2(0.75f, 0.75f);
+        }
+            else if (body.velocity.x < 0)
+            {
+                transform.localScale = new Vector2(-0.75f, 0.75f);
+            }
+        
+    }
 }
